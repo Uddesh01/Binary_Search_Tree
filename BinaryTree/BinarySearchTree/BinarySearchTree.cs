d2333852c173ba9b4c3d3756ad012c5e798c5b36
Uddesh <uddeshpatil2001@gmail.com>
2023-12-22 15:12:07 +0530

    Add: Insert Method

---
+++ BinaryTree/BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,59 @@
+﻿namespace BinaryTree
+{
+    public class BinarySearchTree
+    {
+        private Node root;
+
+        public void insert(int value)
+        {
+            Node temp = root;
+
+            Node node = new Node(value);
+            if (temp == null)
+            {
+                root = node;
+                return;
+            }
+            while (temp != null)
+
+            {
+
+                if (node.value < temp.value)
+                {
+                    if (temp.Left == null)
+                    {
+                        temp.Left = node;
+                        return;
+                    }
+                    temp = temp.Left;
+                }
+                else if (node.value > temp.value)
+                {
+                    if (temp.Right == null)
+                    {
+                        temp.Right = node;
+                        return;
+                    }
+                    temp = temp.Right;
+                }
+                else
+                {
+                    Console.WriteLine("value is already present");
+                }
+            }
+        }
+
+       
+        private class Node
+        {
+            public int value;
+            public Node Left;
+            public Node Right;
+
+            public Node(int value)
+            {
+                this.value = value;
+            }
+        }
+    }
+}
---
+++ BinaryTree/BinarySearchTree/Program.cs
@@ -0,0 +1,19 @@
+﻿using BinaryTree;
+using System.ComponentModel.Design.Serialization;
+using System.Runtime.InteropServices;
+class Program
+{
+    static void Main(string[] args)
+    {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(1);
+        bst.insert(2);
+        bst.insert(3);
+        bst.insert(4);
+        bst.insert(5);
+        bst.insert(6);
+        bst.insert(7);
+
+       
+    }
+}
